a75f8110f67017eb1c2dc60ce0c4616977e49840
PatS4050 <patrick@straatman.eu>
2026-05-29 14:16:27 +0200

    Part 2 opgave 3 gestart. troubles declareren data

--- src/pages/posts/Posts.jsx
+++ src/pages/posts/Posts.jsx
@@ -1,7 +1,8 @@
 import './Posts.css';
 import {Link} from "react-router-dom";
-import posts from '../../constants/data.json';
+// import posts from '../../constants/data.json';
 import {useEffect, useState} from "react";
+import axios from "axios";
 
 
 // const stories= [
@@ -11,18 +12,46 @@ import {useEffect, useState} from "react";
 // ];
 
 // console.log(posts)
+const linkApi = 'https://novi-backend-api-wgsgz.ondigitalocean.app/api/blogposts';
+//project id = 'novi-education-project-id': 'c28ee213-3929-411e-8859-3b773da0246e'
+
 
 function Posts() {
-    const[allBlogPosts, setAllBlogPosts] = useState({})
+
+    const[allBlogPosts, setAllBlogPosts] = useState([])
+    const [error, toggleError] = useState(false)
+    const [loading, toggleLoading] = useState(false)
+
+    async function fetchBlogPosts () {
+        toggleLoading(true);
+        toggleError(false);
+        try {
+// Hier haal je data op
+            const responsePosts = await axios.get(linkApi, {
+                headers: {
+                    'novi-education-project-id': 'c28ee213-3929-411e-8859-3b773da0246e'
+                }
+            });
+            setAllBlogPosts(responsePosts)
+            console.log("data is opgehaald")
+        } catch (e) {
+            toggleError(true)
+            console.error(e);
+// Hier behandel je de errors
+        } finally {
+            toggleLoading(false);
+        }
+    }
+
 
     useEffect(() => {
-        setAllBlogPosts(posts);
-        console.log('Het laden van de database is gelukt', setAllBlogPosts)
+        void fetchBlogPosts()
     }, []);
+
     return (
         <div className="overzichtPage">
             <h1>Overzicht</h1>
-            {posts.map((post) => (
+            {allBlogPosts.map((post) => (
                 <article key={post.id}>
                     <Link to={`/posts/${post.id}`}>
                         <article>
